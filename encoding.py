ff67873f0c6e6dc07591e6b2ba46b09fdf06b081
Hilder Vitor Lima Pereira <hilder.vitor@gmail.com>
2017-02-09 17:58:41 -0200

    Created some tests to the multiplication

--- encoding.py
+++ encoding.py
@@ -2,6 +2,10 @@ from liblll import *
 from math import sqrt
 import sys
 
+def approx_equal(complex1, complex2, delta = 0.00000001):
+    return (complex2.real - delta <= complex1.real and complex1.real <= complex2.real + delta)\
+    and (complex2.imag - delta <= complex1.imag and complex1.imag <= complex2.imag + delta)
+
 def swap_columns(B, i, j):
     if i != j:
         for k in range(len(B)):
@@ -36,6 +40,17 @@ def find_first_column_with_nonzero_at_nth_position(B):
         if B[n_minus_one][j] != 0:
             return j
     return -1
+
+def multiply_as_polynomial(vec1, vec2):
+    p = vec1
+    q = vec2
+    n = len(p)
+    m = len(q)
+    resp = [0 for i in range(n + m - 1)]
+    for i in range(n):
+        for j in range(m):
+            resp[i + j] += p[i] * q[j]
+    return resp
 
 def print_as_polynomial(vec):
     null_poly = True
@@ -118,24 +133,96 @@ def decode(vec, N):
         pow_root_unity *= root_unity
     return val
 
+def test_encoding_decoding(alpha, C, T, n):
+    print "Encoding ", alpha
+
+    v = encode(alpha, C, T, n)
+
+    print "Encoded to: "
+    print_as_polynomial(v)
+
+    print "Decoded to:"
+    decoded_value = decode(v, n)
+    print decoded_value
+
+    print "<Original value>  -  <Decoded value>"
+    print alpha - decoded_value
+
+    return approx_equal(alpha, decoded_value)
+
+def test_example_from_paper(C, T, n):
+    print "--------------\nTesting example from paper.\n-----------------"
+    alpha = 0.655981733221013+0.923883055400882j  # example from the paper
+    return test_encoding_decoding(alpha, C, T, n)
+
+def test_multplication(val1, val2, C, T, n):
+    print "--------------\nTesting multiplication.\n-----------------"
+    print "Encoding val1=", val1
+    p1 = encode(val1, C, T, n)
+    print "Encoded to: "
+    print_as_polynomial(p1)
+
+    print "Encoding val2=", val2
+    p2 = encode(val2, C, T, n)
+    print "Encoded to: "
+    print_as_polynomial(p2)
+
+    prod = multiply_as_polynomial(p1, p2)
+    print "Product decoded to:"
+    decoded_value = decode(prod, n)
+    print decoded_value
+
+    print "Expected product:"
+    expected_value = val1 * val2
+    print expected_value
+
+    print "<Expected value>  -  <Decoded value>"
+    print expected_value - decoded_value
+
+    return approx_equal(expected_value, decoded_value)
+
+
+def test_multplication_several_values(values, C, T, n):
+    M = len(values)
+    polys = [[0] for i in range(M)]
+    print "--------------\nTesting multiplication of %d values.\n-----------------" % M
+    for i in range(M):
+        print "Encoding ", values[i]
+        polys[i] = encode(values[i], C, T, n)
+        print "Encoded to: "
+        print_as_polynomial(polys[i])
+
+    poly_prod = multiply_as_polynomial(polys[0], polys[1])
+    complex_prod = values[0] * values[1]
+    for i in range(2, M):
+        poly_prod = multiply_as_polynomial(poly_prod, polys[i])
+        complex_prod = complex_prod * values[i]
+
+    print "Product decoded to:"
+    decoded_value = decode(poly_prod, n)
+    print decoded_value
+
+    print "Expected product:"
+    expected_value = complex_prod
+    print expected_value
+
+    print "<Expected value>  -  <Decoded value>"
+    print expected_value - decoded_value
+
+    return approx_equal(expected_value, decoded_value)
+
+
 
 C = 10**10
 T = 10
 n = 16
 alpha = 0.655981733221013+0.923883055400882j  # example from the paper
-print "Encoding ", alpha
+beta  = 0.981733221013+0.3288307554812j  # example from the paper
 
-v = encode(alpha, C, T, n)
+print test_example_from_paper(C, T, n), "\n\n"
+print test_encoding_decoding(alpha, C, T, n), "\n\n"
+print test_encoding_decoding(-alpha, C, T, n), "\n\n"
+print test_multplication(alpha, beta, C, T, n), "\n\n"
+values = [alpha, beta, alpha, beta, alpha]
+print test_multplication_several_values(values, C, T, n), "\n\n"
 
-print "Encoded to: "
-print_as_polynomial(v)
-
-print "Decoded to:"
-decoded_value = decode(v, n)
-print decoded_value
-
-print "<Original value>  -  <Decoded value>"
-print alpha - decoded_value
-
-print "<Original value> /  <Decoded value>"
-print alpha / decoded_value
